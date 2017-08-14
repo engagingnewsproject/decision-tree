f14dc183bee9024cf8865ce85cbf6613ff4547c9
Jerry <jones.jeremydavid@gmail.com>
2017-08-14 14:47:13 -0500

    ignoring tree config files

--- tree-config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define('TREE_PATH', '/Users/jj/Dropbox/mamp/sites/decision-tree');
-define('TREE_URL', 'http://dev/decision-tree');
--- tree-db.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('TREE_DB_HOST', 'localhost');
-define('TREE_DB_NAME', 'tree');
-define('TREE_DB_USER', 'root');
-define('TREE_DB_PASSWORD', 'root');
-// define('TREE_DB_PATH', 'database_config');
