a5c2ef96b89c7431056b43159adf1c0967d15fe3
Jerry <jones.jeremydavid@gmail.com>
2018-06-13 11:34:40 -0500

    Use local files instead of CDN for editor js

--- views/edit/footer.php
+++ views/edit/footer.php
@@ -1,10 +1,10 @@
 
 </main>
 <!-- consuming API -->
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="/views/edit/dist/js/axios.js"></script>
 <!-- development version, includes helpful console warnings -->
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.7/sortable.min.js"></script>
+<script src="/views/edit/dist/js/vue.js"></script>
+<script src="/views/edit/dist/js/sortable.js"></script>
 <script type="text/javascript">
 // set the tree var
 var tree = <?php echo json_encode($tree->array());?>
